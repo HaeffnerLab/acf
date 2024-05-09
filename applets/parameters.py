69864c7cfffe5f72cbe522bc097b4f8b03914e0a
David Miron <miron003@me.com>
2024-05-09 16:20:05 -0700

    Add support for nested parameters

--- applets/parameters.py
+++ applets/parameters.py
@@ -75,8 +75,8 @@ class ParameterWidget(QMainWindow):
             self.updating_item_from_datasets = False
             return
 
-        name_item = self.model.item(item.row(), item.column() - 1)
-        dataset_name = f"__param__{name_item.text()}"
+        param_name = item.data()
+        dataset_name = f"__param__{param_name}"
         value = item.text()
         if value.replace(".", "").isnumeric():
             value = float(value)
@@ -107,6 +107,36 @@ class ParameterWidget(QMainWindow):
             param_name = mod["key"].removeprefix("__param__")
             param_value = mod["value"][1]
 
+            # Find item who's data matches the parameter name
+            item = self.find_matching_item(param_name)
+
+            if item is None:
+                self.add_param_to_model(param_name, param_value)
+            else:
+                self.updating_item_from_datasets = True
+                item.setText(str(param_value))
+
+
+            """for row in range(self.model.rowCount()):
+                item = self.model.item(row, 1)
+                print("AAA")
+                print(item)
+                if item is not None:
+                    print(item.text())
+                    print(item.data())
+                print(param_name)
+                print()
+                if item is not None and item.data() == param_name:
+                    found_item = True
+                    self.updating_item_from_datasets = True
+                    item.setText(str(param_value))
+                    break
+
+            if not found_item:
+                print("DIDNT FIND")
+                self.add_param_to_model(param_name, param_value)"""
+
+            """
             # Get the item corresponding to the name of the parameter
             model_name_items = self.model.findItems(param_name, flags=Qt.Qt.MatchExactly, column=0)
 
@@ -126,15 +156,63 @@ class ParameterWidget(QMainWindow):
                 model_value_item.setText(str(param_value))
 
             else:
-                raise RuntimeError(f"Found {len(model_name_items)} items in model with name {param_name}, should be 0 or 1.")
+                raise RuntimeError(f"Found {len(model_name_items)} items in model with name {param_name}, should be 0 or 1.")"""
+
         elif mod["action"] == "delitem":
             param_name = mod["key"].removeprefix("__param__")
+            item = self.find_matching_item(param_name)
+            if item is not None:
+                parent = item.parent()
+                if parent is None:
+                    # Top level elements have parent as None, so remove from the model
+                    self.model.takeRow(item.row())
+                else:
+                    item.parent().takeRow(item.row())
+
+            """for row in range(self.model.rowCount()):
+                item = self.model.item(row, 1)
+                if item.data() == param_name:
+                    self.model.takeRow(row)
+                    break"""
+
+            """
             model_name_items = self.model.findItems(param_name, flags=Qt.Qt.MatchExactly, column=0)
             if len(model_name_items) == 1:
                 self.model.takeRow(model_name_items[0].row())
             if len(model_name_items) > 1:
                 raise RuntimeError("More than 1 row matches item to be deleted.")
+            """
+    def find_matching_item(self, data):
+        """Find the first item in the model tree who's item data matches `data`.
 
+        Since parameters can be nested to arbitraty levels, this works recursively.
+
+        Args:
+            data (?): The data to compare to model leaves.
+
+        Returns: The first item that matches, or None if there are no matches.
+        """
+
+        def recurse(item):
+
+            for row in range(item.rowCount()):
+
+                if item.child(row, 1) is None:
+                    recurse_result = recurse(item.child(row, 0))
+                    if recurse_result is None:
+                        continue
+                    else:
+                        return recurse_result
+
+                elif item.child(row, 1).data() == data:
+                    return item.child(row, 1)
+
+                else:
+                    continue
+
+            return None
+
+        return recurse(self.model.invisibleRootItem())    
 
     def add_param_to_model(self, name, value):
         """Add a parameter to the model to be displayed.
@@ -143,10 +221,78 @@ class ParameterWidget(QMainWindow):
             name (str): The name of the parameter Should not include the parameter manager prefix.
             value (?): The value of the parameter.
         """
-        name_item = QStandardItem(name)
-        name_item.setEditable(False)
+        levels = name.split("/")
         value_item = QStandardItem(str(value))
-        self.model.appendRow([name_item, value_item])
+        value_item.setData(name)
+        print()
+        print(f"Adding param: '{name}'")
+
+        if len(levels) == 1:
+            name_item = QStandardItem(name)
+            name_item.setEditable(False)
+            self.model.appendRow([name_item, value_item])
+
+        else:
+
+            # Parameter has sublevels, so nest them in the model
+
+            # First find the level (where in a/b/c) the parameter name is different
+            # from a tree that already exists
+            #curr_level = 1
+            #while levels[curr_level] == curr_existing
+
+            curr_level = 0
+            curr_item = self.model.invisibleRootItem()
+            while True:
+                """print("Item text")
+                print(curr_item.text())
+                if not curr_item.hasChildren():
+                    break"""
+
+                print(f"rowCount: {curr_item.rowCount()}")
+                found_item = False
+                for row in range(curr_item.rowCount()):
+                    item = curr_item.child(row, 0)
+                    print(f"item text: {item.text()}")
+                    if item.text() == levels[curr_level]:
+                        curr_level += 1
+                        curr_item = item
+                        found_item = True
+                        break
+
+                # If we didn't find the item, exit the while loop
+                if not found_item:
+                    break
+
+            # curr_level is now set to the level in the nested parameter name that does
+            # not match an existing nested parameter in the model
+            print("curr_level")
+            print(curr_level)
+
+            # Start inserting where the parameter nested structure differed
+            previous_item = curr_item
+            for level_name in levels[curr_level:-1]:
+                level_item = QStandardItem(level_name)
+                level_item.setEditable(False)
+                previous_item.appendRow(level_item)
+                previous_item = level_item
+
+            last_name_item = QStandardItem(levels[-1])
+            last_name_item.setEditable(False)
+            previous_item.appendRow([last_name_item, value_item])
+
+
+            """top_level_item = QStandardItem(levels[0])
+            previous_item = top_level_item
+            for level_name in levels[1:-1]:
+                level_item = QStandardItem(level_name)
+                level_item.setEditable(False)
+                previous_item.appendRow(level_item)
+                previous_item = level_item
+            last_name_item = QStandardItem(levels[-1])
+            last_name_item.setEditable(False)
+            previous_item.appendRow([last_name_item, value_item])
+            self.model.appendRow(top_level_item)"""
 
 
 class GetParametersApplet(SimpleApplet):
